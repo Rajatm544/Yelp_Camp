9ecb2075fc0a9787891143438cf36146d5c10294
Rajatm544 <rajatm544@gmail.com>
2019-09-28 11:22:01 +0530

    add mongodb atlas

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var authRoutes		  = require("./routes/index"),
 	campgroundsRoutes = require("./routes/campgrounds");
 
 // Connect mongodb
-mongoose.connect("mongodb+srv://Rajat:3eJLEM6hAyoCJcrP@cluster0-vloon.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Rajat:NH7eQ1BZUYli1JQ8@cluster0-vloon.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 // Set ejs as the default template format; ejs-embedded JS
 app.set("view engine",  "ejs");
 // Tell express that style sheets are in the "public" folder
